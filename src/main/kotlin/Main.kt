56cb272a3568d9822b523924cf417d7d88e75f46
HP <solovev.aleksandr@phystech.edu>
2024-05-15 18:05:02 +0300

    Сделал class для матриц, подправил тип на double

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,33 @@
 package org.example
 
+class Matrix(val rows: List<List<Double>>) {
+    operator fun times(other: Matrix): Matrix {
+        if (this.rows[0].size != other.rows.size) {
+            throw IllegalArgumentException("Число столбцов первой матрицы должно быть равно числу строк второй матрицы.")
+        }
+
+        val result = mutableListOf<List<Double>>()
+
+        for (i in this.rows.indices) {
+            val rowResult = mutableListOf<Double>()
+
+            for (j in other.rows[0].indices) {
+                var sum = 0.0
+
+                for (k in this.rows[0].indices) {
+                    sum += this.rows[i][k] * other.rows[k][j]
+                }
+
+                rowResult.add(sum)
+            }
+
+            result.add(rowResult)
+        }
+
+        return Matrix(result)
+    }
+}
+
 fun main(args: Array<String>) {
     if (args.size != 2) {
         println("Ошибка: Необходимо передать две матрицы.")
@@ -14,25 +42,20 @@ fun main(args: Array<String>) {
         return
     }
 
-    if (matrix1[0].size != matrix2.size) {
-        println("Ошибка: Число столбцов первой матрицы должно быть равно числу строк второй матрицы.")
-        return
-    }
-
-    val result = multiplyMatrices(matrix1, matrix2)
+    val result = matrix1 * matrix2
     printMatrix(result)
 }
 
-fun parseMatrix(input: String): List<List<Int>>? {
+fun parseMatrix(input: String): Matrix? {
     val rows = input.split(";")
-    val matrix = mutableListOf<List<Int>>()
+    val matrix = mutableListOf<List<Double>>()
 
     for (row in rows) {
         val numbers = row.split(" ")
-        val numberList = mutableListOf<Int>()
+        val numberList = mutableListOf<Double>()
 
         for (number in numbers) {
-            val num = number.toIntOrNull()
+            val num = number.toDoubleOrNull()
             if (num == null) {
                 return null
             } else {
@@ -47,33 +70,11 @@ fun parseMatrix(input: String): List<List<Int>>? {
         matrix.add(numberList)
     }
 
-    return matrix
+    return Matrix(matrix)
 }
 
-fun multiplyMatrices(matrix1: List<List<Int>>, matrix2: List<List<Int>>): List<List<Int>> {
-    val result = mutableListOf<List<Int>>()
-
-    for (i in matrix1.indices) {
-        val rowResult = mutableListOf<Int>()
-
-        for (j in matrix2[0].indices) {
-            var sum = 0
-
-            for (k in matrix1[0].indices) {
-                sum += matrix1[i][k] * matrix2[k][j]
-            }
-
-            rowResult.add(sum)
-        }
-
-        result.add(rowResult)
-    }
-
-    return result
-}
-
-fun printMatrix(matrix: List<List<Int>>) {
-    for (row in matrix) {
+fun printMatrix(matrix: Matrix) {
+    for (row in matrix.rows) {
         println(row.joinToString(" "))
     }
 }
